2d063ec2bb6c3dd3a226cf04747ff1b125ad2518
Simon Cropper <simon.p.cropper@gmail.com>
2023-04-01 06:28:26 +0200

    added user card

--- pages/login.vue
+++ pages/login.vue
@@ -1,13 +1,43 @@
 <template>
   <div class="prose w-full max-w-2xl h-9">
     <h1>Login to {{ title }}</h1>
-    <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded">
+    <button
+      @click="login"
+      class="bg-blue-500 text-white font-bold py-2 px-4 rounded">
       Login with Github
     </button>
 
   </div>
 </template>
 
-<script setup>
+<script setup lang ="ts">
 const { title } = useCourse();
+
+const supabase = useSupabaseClient();
+
+const login = async () => {
+  const error = ref();
+  try {
+    const { error: signInError } = await supabase.auth.signInWithOAuth(
+      {
+        provider: 'github',
+      });
+    console.log('here 1');
+    const user = useSupabaseUser();
+    console.log('user', user);
+
+    if (signInError) {
+      error.value = signInError;
+      console.log(signInError);
+      console.log('here 1');
+    }
+  } catch (err) {
+    {
+      console.log('here 1');
+      error.value = err;
+      console.log(err);
+    }
+  }
+};
+
 </script>
